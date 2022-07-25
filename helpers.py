63b12f0ad65d742039a1149179a0fd4d52b7e1eb
Zafer Balkan <zafer@zaferbalkan.com>
2022-07-25 22:28:09 +0300

    Fixed helper imports

--- helpers.py
+++ helpers.py
@@ -1,6 +1,6 @@
 # dt.timestamp() returns UTC time as expected by the LDAP server
-import datetime
-from constants import constants as const
+from datetime import datetime
+import constants as const
 
 def dt_to_filetime(dt: datetime) -> int:
     return int((dt.timestamp() + const.EPOCH_TIMESTAMP) * const.HUNDREDS_OF_NANOSECONDS)
