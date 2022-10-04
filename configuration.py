5a9e857f703dc578d02f78e3a9b083b8b865db00
Zafer Balkan <zafer@zaferbalkan.com>
2022-10-04 21:55:39 +0300

    Refactor configuration

--- configuration.py
+++ configuration.py
@@ -1,6 +1,5 @@
-from typing import List, Optional
-from typing import Any
 from dataclasses import dataclass
+from typing import Any, List, Optional
 
 
 @dataclass
@@ -18,10 +17,10 @@ class CfgServer:
 
     @staticmethod
     def from_dict(obj: Any) -> 'CfgServer':
-        _address = str(obj.get("address"))
-        _port = int(obj.get("port"))
-        _gc_port = int(obj.get("port"))
-        _ssl = False
+        _address: str = str(obj.get("address"))
+        _port: int = int(obj.get("port"))
+        _gc_port: int = int(obj.get("port"))
+        _ssl: bool = False
         return CfgServer(_address, _port, _ssl, _gc_port)
 
 
@@ -41,18 +40,19 @@ class RunnerConfig:
 
     @staticmethod
     def from_dict(obj: Any) -> 'RunnerConfig':
-        _server = [CfgServer.from_dict(y) for y in obj.get("server")]
-        _domain = str(obj.get("domain"))
-        _cred_cache_file = str(obj.get("cred-cache-file"))
-        _client_keytab_file = str(obj.get("client-keytab-file"))
-        _ldap_attribute_password = str(obj.get("ldap-attribute-password"))
-        _ldap_attribute_password_expiry = str(
+        _server: list[CfgServer] = [
+            CfgServer.from_dict(y) for y in obj.get("server")]
+        _domain: str = str(obj.get("domain"))
+        _cred_cache_file: str = str(obj.get("cred-cache-file"))
+        _client_keytab_file: str = str(obj.get("client-keytab-file"))
+        _ldap_attribute_password: str = str(obj.get("ldap-attribute-password"))
+        _ldap_attribute_password_expiry: str = str(
             obj.get("ldap-attribute-password-expiry"))
-        _hostname = str(obj.get("hostname"))
-        _password_change_user = str(obj.get("password-change-user"))
-        _password_days_valid = int(obj.get("password-days-valid"))
-        _password_length = int(obj.get("password-length"))
-        _password_alphabet = str(obj.get("password-alphabet"))
+        _hostname: str = str(obj.get("hostname"))
+        _password_change_user: str = str(obj.get("password-change-user"))
+        _password_days_valid: int = int(obj.get("password-days-valid"))
+        _password_length: int = int(obj.get("password-length"))
+        _password_alphabet: str = str(obj.get("password-alphabet"))
         return RunnerConfig(_server, _domain, _cred_cache_file, _client_keytab_file, _ldap_attribute_password, _ldap_attribute_password_expiry, _hostname, _password_change_user, _password_days_valid, _password_length, _password_alphabet)
 
 
@@ -65,10 +65,10 @@ class CfgLdapAttributes:
 
     @staticmethod
     def from_dict(obj: Any) -> 'CfgLdapAttributes':
-        _OperatingSystem = str(obj.get("Operating System"))
-        _lastLogonTimestamp = str(obj.get("lastLogonTimestamp"))
-        _AdministratorPassword = str(obj.get("Administrator Password"))
-        _PasswordExpirationDate = str(obj.get("Password Expiration Date"))
+        _OperatingSystem: str = str(obj.get("Operating System"))
+        _lastLogonTimestamp: str = str(obj.get("lastLogonTimestamp"))
+        _AdministratorPassword: str = str(obj.get("Administrator Password"))
+        _PasswordExpirationDate: str = str(obj.get("Password Expiration Date"))
         return CfgLdapAttributes(_OperatingSystem, _lastLogonTimestamp, _AdministratorPassword, _PasswordExpirationDate)
 
     def to_dict(self) -> dict[str, str]:
@@ -91,12 +91,13 @@ class ClientConfig:
 
     @staticmethod
     def from_dict(obj: Any) -> 'ClientConfig':
-        _server = [CfgServer.from_dict(y) for y in obj.get("server")]
-        _domain = str(obj.get("domain"))
-        _username = str(obj.get("username"))
-        _ldap_attribute_password = str(obj.get("ldap-attribute-password"))
-        _ldap_attribute_password_expiry = str(
+        _server: list[CfgServer] = [
+            CfgServer.from_dict(y) for y in obj.get("server")]
+        _domain: str = str(obj.get("domain"))
+        _username: str = str(obj.get("username"))
+        _ldap_attribute_password: str = str(obj.get("ldap-attribute-password"))
+        _ldap_attribute_password_expiry: str = str(
             obj.get("ldap-attribute-password-expiry"))
-        _ldap_attributes = CfgLdapAttributes.from_dict(
+        _ldap_attributes: CfgLdapAttributes = CfgLdapAttributes.from_dict(
             obj.get("ldap-attributes"))
         return ClientConfig(_server, _domain, _username, _ldap_attribute_password, _ldap_attribute_password_expiry, _ldap_attributes)
